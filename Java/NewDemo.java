45e926aaf5cf9ab0447b45869f4f6de56815aa68
chen <sb42156321@gmail.xom>
2018-12-05 15:32:14 +0800

    换成markdown来记录

---
+++ Java/NewDemo.java
@@ -0,0 +1,13 @@
+/**
+ * NewDemo
+ */
+public class NewDemo {
+
+    public static void main(String[] args) {
+        System.out.println("newdemo");
+        String pen = new String();
+        pen = "sdfasdfasdf";
+        pen = pen.substring(3);
+        System.out.println(pen);
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 ###这是一个关于记录的的世界
 **试试加粗功能**
 >引用一些东西
+>
 >换个行试试
 ----
+```
+public class demo
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+```
